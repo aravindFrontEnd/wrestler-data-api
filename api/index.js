2c1ebcbda8e616823f197d141a45cd13b3148200
Aravind G <aravind.g@ibm.com>
2024-12-23 10:07:05 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -2,16 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Manual CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
+});
+
 // Serve the wrestler data
 const wrestlerData = require('../wrestlerData');
 
-// Serve the wrestler data
 app.get('/api/wrestlers', (req, res) => {
   res.json(wrestlerData);
 });
 
-// Serve the static assets (images)
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Export the Express app
 module.exports = app;
